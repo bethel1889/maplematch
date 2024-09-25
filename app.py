0755e13f89985de8114e4b1f377fbc02f53d39ec
bethel1889 <ibeaghaemmanuel@gmail.com>
2024-09-25 09:14:27 +0100

    Test commit

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-
 from helpers import apology, login_required, region_check, program_stream_check, check, REGIONS, PROGRAM_STREAMS, region_numbers, program_stream_numbers
 
 # Configure application
